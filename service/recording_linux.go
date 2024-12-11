fec0eda0fb0aedfdf65c26a9d69c9239bdb2e463
blnidson76 <blnidson76.gmail.com>
2024-12-11 16:04:32 +0300

    playback linux

--- service/recording_linux.go
+++ service/recording_linux.go
@@ -6,3 +6,10 @@ func (s *RecordingService) rec_start() error {
 func (s *RecordingService) rec_stop() error {
 	return nil
 }
+
+func (s *RecordingService) playback_start(clip string) error {
+	return nil
+}
+func (s *RecordingService) playback_stop() error {
+	return nil
+}
